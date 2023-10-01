45bcf76a06e893ae04c560f4575d7841430d480b
cvaize <d.orlov@2uplab.com>
2023-10-01 07:57:06 +0500

    Html/Admin/Default Working

--- app/Http/Controllers/Html/Admin/Default/DashboardController.php
+++ app/Http/Controllers/Html/Admin/Default/DashboardController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers\Html\Admin\Default;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-
+    public function __invoke(Request $r): View|\Illuminate\Foundation\Application|Factory|Application
+    {
+        return view('html.admin.default.pages.dashboard');
+    }
 }
---
+++ resources/views/html/admin/default/pages/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('html.admin.default.layouts.app')
+
+@section('content')
+    123
+@endsection
